33cee30eecd94a8b80dc5826da1cb46e98746e6c
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-14 19:19:15 +0100

    Create Form component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,9 @@
+export const Form = (): JSX.Element => {
+  return (
+    <section className="form" aria-label="create a task">
+      <label htmlFor="todo">What do I need to do?</label>
+      <input id="todo" type="text" placeholder="My thingies" />
+      <button type="submit">Create a task!</button>
+    </section>
+  );
+};
