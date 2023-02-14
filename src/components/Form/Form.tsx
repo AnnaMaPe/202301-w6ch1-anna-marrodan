ef888d7fcbf84b924031a3268a2fe3a1ff101889
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-14 19:47:54 +0100

    Modify BEM in Form component to proper structure

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,9 +1,18 @@
 export const Form = (): JSX.Element => {
   return (
     <form className="form" aria-label="create a task">
-      <label htmlFor="todo">What do I need to do?</label>
-      <input id="todo" type="text" placeholder="My thingies" />
-      <button type="submit">Create a task!</button>
+      <label className="form__label" htmlFor="todo">
+        What do I need to do?
+      </label>
+      <input
+        className="form__input"
+        id="todo"
+        type="text"
+        placeholder="My thingies"
+      />
+      <button className="form__button" type="submit">
+        Create a task!
+      </button>
     </form>
   );
 };
